d306d79799fddc1d77a2e838c5280ad1671e7776
Elaine <ecf2@cin.ufpe.br>
2020-08-18 11:38:49 -0300

    Initial Commit

---
+++ PelejoAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PelejoAutoLayout
+//
+//  Created by Elaine  Cruz on 18/08/20.
+//  Copyright © 2020 Elaine  Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
