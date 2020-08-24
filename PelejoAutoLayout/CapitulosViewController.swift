521f9ab1bd6f28fc9e95a55c0e02de4e182926f5
Elaine <ecf2@cin.ufpe.br>
2020-08-24 09:58:35 -0300

    Autolayout on fisrt page and collection and table view implemented

---
+++ PelejoAutoLayout/CapitulosViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CapitulosViewController.swift
+//  PelejoAutoLayout
+//
+//  Created by Elaine  Cruz on 18/08/20.
+//  Copyright © 2020 Elaine  Cruz. All rights reserved.
+//
+
+import Foundation
---
+++ PelejoAutoLayout/ConfigViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigViewController.swift
+//  PelejoAutoLayout
+//
+//  Created by Elaine  Cruz on 20/08/20.
+//  Copyright © 2020 Elaine  Cruz. All rights reserved.
+//
+
+import Foundation
